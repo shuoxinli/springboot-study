e9c37320d9a28040532ae84318867b2dbff8e170
hangov <1013818707@qq.com>
2019-03-18 19:00:59 +0800

    commit -m "new study"

--- spring-boot-mybatis/src/main/java/com/springboot/mybatis/controller/Controller.java
+++ spring-boot-mybatis/src/main/java/com/springboot/mybatis/controller/Controller.java
@@ -3,10 +3,8 @@ package com.springboot.mybatis.controller;
 
 import com.springboot.mybatis.entity.Course;
 import com.springboot.mybatis.service.CourseService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.mybatis.util.RandomUtil;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -31,4 +29,16 @@ public class Controller {
     public void deleteCourse(@PathVariable("id") long id){
         courseService.delete(id);
     }
+
+    @RequestMapping(value = "/course",method = RequestMethod.POST)
+    public Course addCourse(@RequestBody Course course){
+        course.setCourseCode(RandomUtil.getRandomCode());
+        return course;
+    }
+
+    @RequestMapping(value = "/course", method = RequestMethod.PUT)
+    public void updateCourse(@RequestBody Course course) {
+        courseService.update(course);
+    }
+
 }
